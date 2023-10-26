721ddefef5c980913bcb159a146d0d7c10141d8d
Swayam Jena <138446357+DemoN0606@users.noreply.github.com>
2023-10-26 20:46:29 +0530

    weather.py

--- weather.py
+++ weather.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from tkinter import *
 import tkinter as tk
 from geopy.geocoders import Nominatim
@@ -7,40 +8,41 @@ from datetime import datetime
 import requests
 import pytz
 
+# Create the main window
 root = tk.Tk()
 root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False, False)
 
+# Define the function to get the weather
 def getWeather():
     try:
-        city=textfield.get()
+        city = textfield.get()
 
+        # Get the location and timezone
         geolocator = Nominatim(user_agent="geoapiExercises")
         location = geolocator.geocode(city)
         obj = TimezoneFinder()
         result = obj.timezone_at(lng=location.longitude,lat=location.latitude)
-    
-        home=pytz.timezone(result)
-        local_time=datetime.now(home)
-        current_time=local_time.strftime("%I:%M:%p")
+        home = pytz.timezone(result)
+        local_time = datetime.now(home)
+        current_time = local_time.strftime("%I:%M:%p")
         clock.config(text=current_time)
         name.config(text="CURRENT WEATHER")
 
-        #weather
-        api="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=647284085143cecd4100f705d64cf11b"
+        # Get the weather data
+        api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=647284085143cecd4100f705d64cf11b"
+        json_data = requests.get(api).json()
+        condition = json_data['weather'][0]['main']
+        description = json_data['weather'][0]['description']
+        temp = int(json_data['main']['temp']-273.15)
+        pressure = json_data['main']['pressure']
+        humidity = json_data['main']['humidity']
+        wind = json_data['wind']['speed']
 
-        json_data=requests.get(api).json()
-        condition=json_data['weather'][0]['main']
-        description=json_data['weather'][0]['description']
-        temp=int(json_data['main']['temp']-273.15)
-        pressure=json_data['main']['pressure']
-        humidity=json_data['main']['humidity']
-        wind=json_data['wind']['speed']
-
+        # Update the labels with the weather data
         t.config(text=(temp,"°"))
         condition_label.config(text=(condition,"|","FEELS","LIKE",temp,"°"))
-
         w.config(text=wind)
         h.config(text=humidity)
         d.config(text=description)
@@ -49,55 +51,46 @@ def getWeather():
     except Exception as e:
         messagebox.showerror("Weather App","City Not Found")
 
-
-# search box
+# Create the search box
 Search_image = PhotoImage(file="search.png")
 myimage = Label(image=Search_image)
 myimage.place(x=20, y=20)
-
 textfield = tk.Entry(root, justify=CENTER, width=17, font=("gilroy", 25, "bold"),bg="#404040", border=0, fg="white")
 textfield.place(x=50, y=40)
 textfield.focus()
-
 Search_icon=PhotoImage(file="search_icon.png")
 myimage_icon=button=Button(image=Search_icon,borderwidth=0,cursor="hand2",bg="#404040",command=getWeather)
 myimage_icon.place(x=400,y=34)
 
-#logo
+# Create the logo
 Logo_image=PhotoImage(file="logo.png")
 logo=Label(image=Logo_image)
 logo.place(x=150,y=100)
 
-#Bottom box
+# Create the bottom box
 Frame_image=PhotoImage(file="box.png")
 frame_myimage=Label(image=Frame_image)
 frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
 
-#time
+# Create the time labels
 name=Label(root,font=("gilroy",15,"bold"))
 name.place(x=30,y=100)
 clock=Label(root,font=("gilroy",15,))
 clock.place(x=30,y=130)
 
-#label
+# Create the weather data labels
 label1=Label(root,text="WIND",font=("gilroy",15,"bold"),bg="#1ab5ef",fg="white")
 label1.place(x=120,y=400)
-
 label2=Label(root,text="HUMIDITY",font=("gilroy",15,"bold"),bg="#1ab5ef",fg="white")
 label2.place(x=250,y=400)
-
 label3=Label(root,text="DESCRIPTION",font=("gilroy",15,"bold"),bg="#1ab5ef",fg="white")
 label3.place(x=430,y=400)
-
 label4=Label(root,text="PRESSURE",font=("gilroy",15,"bold"),bg="#1ab5ef",fg="white")
 label4.place(x=650,y=400)
-
-
 t=Label(font=("gilroy",70,"bold"),fg="#ee666d")
 t.place(x=400,y=150)
 condition_label=Label(font=("gilroy",15,"bold"))
 condition_label.place(x=400,y=250)
-
 w=Label(text="...",font=("gilroy",20,"bold"),bg="#1ab5ef")
 w.place(x=120,y=430)
 h=Label(text="...",font=("gilroy",20,"bold"),bg="#1ab5ef")
@@ -107,4 +100,5 @@ d.place(x=450,y=430)
 p=Label(text="...",font=("gilroy",20,"bold"),bg="#1ab5ef")
 p.place(x=670,y=430)
 
+# Run the main loop
 root.mainloop()
